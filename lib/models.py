493ab8752608e17a6b8b658054ee68184b781eea
Maria Jenkins <macjenkin@gmail.com>
2023-10-13 04:37:24 -0500

    complete challenge

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine, ForeignKey, Table, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -9,6 +9,19 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+company_dev = Table(
+    'company_devs',
+    Base.metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('company_id', ForeignKey('companies.id')),
+    Column('dev_id', ForeignKey('devs.id')),
+    extend_existing=True,
+)
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,8 +29,28 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    devs = relationship('Dev', secondary=company_dev, back_populates='companies')
+    freebies = relationship('Freebie', backref=backref('company'))
+
     def __repr__(self):
         return f'<Company {self.name}>'
+    
+    def give_freebie(self, dev, item_name, value):
+        if isinstance(dev, Dev):
+            new_freebie = Freebie(
+                item_name=item_name,
+                value=value,
+                dev_id=dev.id,
+                company_id=self.id
+            )    
+            session.add(new_freebie)
+            session.commit()
+        else:
+            return TypeError("dev must be an instance of the Dev class.")
+    
+    @classmethod
+    def oldest_company(cls):
+        return session.query(Company).order_by(Company.founding_year).first()
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -25,5 +58,41 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    companies = relationship('Company', secondary=company_dev, back_populates='devs')
+    freebies = relationship('Freebie', backref=backref('dev'))
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def received_one(self, item_name):
+        return item_name in [freebie.item_name for freebie in self.freebies]
+    
+    def give_away(self, dev, freebie):
+        if isinstance(freebie, Freebie):
+            if freebie in self.freebies:
+                if isinstance(dev, Dev):
+                    freebie.dev = dev
+                    session.add(freebie)
+                    session.commit()
+                else:
+                    return TypeError("dev must be an instance of the Dev class")
+            else:
+                return ValueError(f"The {freebie.item_name} freebie does not belong to {self.name}.")
+        else: 
+            return TypeError("freebie must be an instance of the Freebie class.")
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
+    
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,87 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+from random import choice as rc
+import random
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from faker import Faker
+
+from models import Company, Dev, Freebie
+
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+fake = Faker()
+
+swag = ['t-shirt', 'hat', 'water bottle', 'pen', 'sticker', 
+        'keychain', 'notebook', 'mouse pad', 'yoga mat', 'frisbee', 
+        'chapstick', 'lanyard', 'tote bag', 'reusable straw set', 
+        'hoodie', 'koozie', 'mug', 'tumbler', 'beanie', 'socks', 
+        'sunglasses', 'umbrella', 'yo-yo', 'calendar', 'duffle bag']
+
+def create_companies():
+    companies = [
+        Company(
+            name = fake.company(),
+            founding_year = fake.year()
+        ) 
+    for i in range(20)]
+    session.add_all(companies)
+    session.commit()
+    return companies
+
+def create_devs():
+    devs = [
+        Dev(
+            name = fake.name()
+        ) 
+    for i in range(100)]
+    session.add_all(devs)
+    session.commit()
+    return devs
+
+def create_freebies():
+    freebies = [
+        Freebie(
+            item_name = random.choice(swag),
+            value = random.randint(0, 100)
+        ) 
+    for i in range(300)]
+    session.add_all(freebies)
+    session.commit()
+    return freebies
+
+def delete_records():
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+    session.commit()
+
+def relate_companies_to_devs(companies, devs):
+    for company in companies:
+        for i in range(random.randint(1, 10)):
+            dev = random.choice(devs)
+            if company not in dev.companies:
+                dev.companies.append(company)
+                session.add(dev)
+                session.commit()
+    return companies, devs
+
+def relate_freebies(companies, devs, freebies):
+    for freebie in freebies:
+        freebie.company = rc(companies)
+        freebie.dev = rc(devs)
+        
+    session.add_all(freebies)
+    session.commit()
+    return companies, devs, freebies
+
+if __name__ == '__main__':
+    delete_records()
+    companies = create_companies()
+    devs = create_devs()
+    freebies = create_freebies()
+    companies, devs = relate_companies_to_devs(companies, devs)
+    companies, devs, freebies = relate_freebies(companies, devs, freebies)
